a3237a4d736595e6e6e1ba46bf748c78d4cf0317
italoaraujooj <matheus.ramalho1354@gmail.com>
2023-05-08 23:52:56 -0300

    feat: desenvolvimento do aside

--- spotify-tailwind/src/app/layout.tsx
+++ spotify-tailwind/src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className='bg-zinc-800 text-3xl'>{children}</body>
+      <body className='bg-zinc-500 text-xl'>{children}</body>
     </html>
   )
 }
--- spotify-tailwind/src/app/page.tsx
+++ spotify-tailwind/src/app/page.tsx
@@ -1,7 +1,41 @@
+import { Home as HomeIcon, Search, Library, Plus, ArrowRight } from 'lucide-react'
+
 export default function Home() {
   return (
-    <main>
-      <h1 className='font-semibold text-3xl'>Spotify</h1>
-      </main>
-  )
+    <div className='h-screen flex flex-col bg-zinc-950 py-3'>
+      <div className='flex flex-1'>
+        <aside className='w-78 bg-zinc-950 p-2 flex flex-col gap-2'>
+          <div className="w-72 h-24 bg-zinc-900 rounded-xl flex-col align-middle">
+            <nav>
+              <a href="" className='flex items-center gap-2 text-sm font-semibold text-zinc-200 p-3 px-5'>
+                <HomeIcon />
+                Home
+              </a>
+              <a href="" className='flex items-center gap-2 text-sm font-semibold text-zinc-200 p-3 px-5'>
+                <Search />
+                Search
+              </a>
+            </nav>
+          </div>
+          <div className="w-72 h-24 bg-zinc-900 rounded-xl flex-col align-bottom">
+            <nav>
+              <a href="" className='w-72 flex items-center gap-2 text-sm font-semibold text-zinc-200 p-3 px-5'>
+                <Library />
+                Your Library
+                <div className='flex'>
+                  <Plus />
+                  <ArrowRight />
+                </div>
+                
+              </a>
+            </nav>
+          </div>
+        </aside>
+        <main className='flex-1 bg-slate-700/30 rounded-xl p-2 py-3 text-zinc-200'></main>
+      </div>
+      <footer className=' bg-zinc-800 border-t border-zinc-700 px-6'>
+        Footer
+      </footer>
+    </div>
+  );
 }
